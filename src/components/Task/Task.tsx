43aac82a01224afdb8c3c1ceabd1a4bfc8191664
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2025-01-14 21:30:00 +0300

    styles for Task

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,7 +1,31 @@
 import styles from './Task.module.css';
+import icon_favourite from './img/icon_favourite.png';
+import icon_done from './img/icon_done.png';
+import icon_delete from './img/icon_delete.png'
 
 const Task = () => {
-  return <></>;
+  return (
+    <div className={styles.task_container}>
+      <button className={styles.btn_favorite_container}>
+        <img
+          className={styles.btn_favorite_icon}
+          src={icon_favourite}
+          alt="icon_favourite"
+        />
+      </button>
+      <div className={styles.task_description}>
+        Mnput type="text" placeholder="Enter task"{' '}
+      </div>
+      <div className={styles.btn_container}>
+        <button className={styles.btn}>
+          <img className={styles.btn_icon} src={icon_done} alt="icon_done" />
+        </button>
+        <button className={styles.btn}>
+          <img className={styles.btn} src={icon_delete} alt="icon_delete" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Task;
