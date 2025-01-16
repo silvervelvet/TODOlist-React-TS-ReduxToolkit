2ead31ab54ce2ce68a5d798e21465aed1528df16
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2025-01-16 21:06:06 +0300

    html and styles for NewTask

--- src/components/NewTask/NewTask.tsx
+++ src/components/NewTask/NewTask.tsx
@@ -4,9 +4,13 @@ import icon_add from './img/icon_add.png';
 const NewTask = () => {
   return (
     <form className={styles.form_add_task_container}>
-      <input className={styles.input_newtask} type="text" placeholder="Add task..."></input>
+      <input
+        className={styles.input_newtask}
+        type="text"
+        placeholder="Add task..."
+      ></input>
       <button className={styles.btn_add_container}>
-        <img className={styles.icon_btn_add} src={icon_add} alt='icon_add' />
+        <img className={styles.icon_btn_add} src={icon_add} alt="icon_add" />
       </button>
     </form>
   );
