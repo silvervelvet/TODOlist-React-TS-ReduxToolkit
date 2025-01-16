364d528b5ef5c0b5decfc2a6bf790e939073f5b2
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2025-01-17 00:40:28 +0300

    install RTK Query and realize GET request slice

---
+++ src/services/todoApi.ts
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface TaskType {
+  id: string,
+  description: string,
+  status: 'todo' | 'isActive' | 'isDone',
+  isFavourites?: boolean
+}
+
+export const todoApi = createApi({
+  reducerPath: 'todoApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/todos' }),
+  endpoints: (builder) => ({
+    getTodos: builder.query<TaskType[], void>({
+      query: () => 'todos',
+    }),
+    addTodo: builder.mutation<TaskType, string>({
+      query: (text) => ({
+        url: 'todos',
+        method: 'POST',
+        body: { text, completed: false },
+      }),
+    }),
+  }),
+});
+
+export const { useGetTodosQuery, useAddTodoMutation } = todoApi;
