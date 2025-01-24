b75e2093861f9b2cca791857edaac02f036b88a9
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2025-01-25 00:04:41 +0300

    reorganize Tabs grom HomePage into ListTaskk

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,13 @@
 import ListTasks from '../../components/ListTasks';
 import NewTask from '../../components/NewTask';
 import PaginationTasks from '../../components/PaginationTasks';
-import Tabs from '../../components/Tabs';
 import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
     <main className={styles.main}>
       <NewTask />
-      <div className={styles.tabs_list_tasks_container}>
-        <Tabs />
-        <ListTasks />
-      </div>
+      <ListTasks />
       <PaginationTasks />
     </main>
   );
