7a81ad28da1eefc96006300df86db91ec556f280
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2025-01-25 00:05:38 +0300

    add correct logic for Tabs

--- src/components/ListTasks/ListTasks.tsx
+++ src/components/ListTasks/ListTasks.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react'
 import { useGetTodosQuery } from '../../services/todoApi';
+import Tabs from '../Tabs';
 import Task from '../Task';
 import styles from './ListTasks.module.css';
 
 const ListTasks: React.FC = () => {
   const { data: todos = [], error, isLoading } = useGetTodosQuery();
+  const [activeTab, setActiveTab] = useState<
+    'all' | 'completed' | 'pending' | 'favourites'
+  >('all');
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -13,10 +18,24 @@ const ListTasks: React.FC = () => {
     return <div>Error loading tasks</div>;
   }
 
+  const filteredTodos = todos.filter((todo) => {
+    switch (activeTab) {
+      case 'completed':
+        return todo.status === 'isDone';
+      case 'pending':
+        return todo.status === 'isActive';
+      case 'favourites':
+        return todo.isFavourites === true;
+      default:
+        return true;
+    }
+  });
+
   return (
     <section className={styles.list_tasks_container}>
-      {todos.length > 0 ? (
-        todos.map((todo) => <Task key={todo.id} todo={todo} />)
+      <Tabs  activeTab={activeTab} setActiveTab={setActiveTab}/>
+      {filteredTodos.length > 0 ? (
+        filteredTodos.map((todo) => <Task key={todo.id} todo={todo} />)
       ) : (
         <p>No tasks available</p>
       )}
