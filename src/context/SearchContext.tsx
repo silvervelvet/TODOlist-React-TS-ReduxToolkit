90a981d43909d47eb8054e4d1ed7547a7f2717ee
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2025-01-25 17:44:50 +0300

    context for Search

---
+++ src/context/SearchContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+const SearchContext = createContext<
+  { query: string; setQuery: (query: string) => void } | undefined
+>(undefined);
+
+interface SearchProviderProps {
+  children: ReactNode;
+}
+
+export const SearchProvider: React.FC<SearchProviderProps> = ({ children }) => {
+  const [query, setQuery] = useState('');
+
+  return (
+    <SearchContext.Provider value={{ query, setQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearchContext = () => {
+  const context = useContext(SearchContext);
+  if (!context) {
+    throw new Error('useSearchContext must be used within a SearchProvider');
+  }
+  return context;
+};
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,12 +1,13 @@
 import Header from '../../components/Header';
+import { SearchProvider } from '../../context/SearchContext';
 import HomePage from '../HomePage';
 
 function App() {
   return (
-    <>
+    <SearchProvider>
       <Header />
       <HomePage />
-    </>
+    </SearchProvider>
   );
 }
 
